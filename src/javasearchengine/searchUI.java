c33d4cb65d1842291356f8a5c7ee4c9bc7821d59
HasanTuncelCoban <hasantuncelcoban@gmail.com>
2020-06-18 16:06:50 +0300

    fonksiyon eklendi
    arama yapan parametrelere fonksiyon ekleyerek yeniden düzenlendi

--- src/javasearchengine/searchUI.java
+++ src/javasearchengine/searchUI.java
@@ -29,8 +29,8 @@ import javax.swing.SwingConstants;
 public class searchUI extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField searchTxt;
-	private JTextArea contentsTxt;
+	private static JTextField searchTxt;
+	private static JTextArea contentsTxt;
 	private JScrollPane scrollPane;
 
 	/**
@@ -53,7 +53,8 @@ public class searchUI extends JFrame {
 	 * Create the frame.
 	 */
 	public searchUI() {
-		setTitle("Java Text Retrival System");
+		setResizable(false);
+		setTitle("Document Retrieval System");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 647, 517);
 		contentPane = new JPanel();
@@ -83,60 +84,6 @@ public class searchUI extends JFrame {
 		// scrollPane.add(contentsTxt);
 		contentPane.add(scrollPane);
 		// contentPane.add(contentsTxt);
-
-		JButton srchBTN = new JButton("SEARCH");
-		srchBTN.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				
-				//String temp="<TITLE>"+searchTxt.getText()+ "</TITLE>";
-				
-				
-               try {
-            	   for(int i=0;i<22;i++) {
-            			BufferedReader reader= new BufferedReader(new FileReader("src\\\\javasearchengine/read/reut2-"+i+".sgm"));
-            			StringBuilder strB=new StringBuilder();
-            			String str= "";
-            			StringBuilder testB=new StringBuilder();
-            			String temp="<TITLE>"+searchTxt.getText()+ "</TITLE>";
-            			//String temp="<TITLE>AMERICAN EXCHANGE INTRODUCES INSTITUTIONAL INDEX</TITLE>";HELLO WORLD
-            			String body= "";
-            			
-            			do {
-            				str =reader.readLine();
-            				if(str==null)break;
-            						if(str.contentEquals(temp)) {
-            							strB.append(str);
-            							//contentsTxt.setText(strB.toString());
-            								do {									
-            								body=reader.readLine();
-            								if(body==null)break;
-            								testB.append(body);
-            								testB.append("\n");	
-            								ArrayList<String> bodyContents = new ArrayList<>();
-            								bodyContents.add(testB.toString());
-            												//System.out.println(body.toString());
-            								contentsTxt.setText(bodyContents.toString());
-            												if(body.contentEquals("&#3;</BODY></TEXT>")==true)break;	
-            								}while(body!=null);	
-            										
-            						}
-            			}while(str!=null);
-            			
-            	   }
-					}catch(Exception e1) {
-						JOptionPane.showInternalMessageDialog(null, "Hata"+e1.toString());
-					}
-			}
-		});
-		srchBTN.setFont(new Font("Calibri", Font.BOLD, 15));
-		srchBTN.setBounds(396, 39, 108, 38);
-		contentPane.add(srchBTN);
-
-		JLabel lblNewLabel = new JLabel("Resault :");
-		lblNewLabel.setForeground(Color.RED);
-		lblNewLabel.setFont(new Font("Calibri", Font.BOLD, 15));
-		lblNewLabel.setBounds(59, 146, 55, 19);
-		contentPane.add(lblNewLabel);
 		
 		JRadioButton date = new JRadioButton("Date");
 		date.setHorizontalAlignment(SwingConstants.RIGHT);
@@ -144,21 +91,180 @@ public class searchUI extends JFrame {
 		date.setBounds(167, 90, 54, 25);
 		contentPane.add(date);
 		
-		JRadioButton rdbtnNewRadioButton = new JRadioButton("Topics");
-		rdbtnNewRadioButton.setFont(new Font("Calibri", Font.BOLD, 14));
-		rdbtnNewRadioButton.setBounds(223, 92, 65, 21);
-		contentPane.add(rdbtnNewRadioButton);
+		JRadioButton topics = new JRadioButton("Topics");
+		topics.setFont(new Font("Calibri", Font.BOLD, 14));
+		topics.setBounds(223, 92, 65, 21);
+		contentPane.add(topics);
+
+		JButton srchBTN = new JButton("SEARCH");
+		srchBTN.setBackground(Color.GREEN);
+		srchBTN.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//Thread mt =new Thread(new searchUI());
+				//mt.start();
+				if(date.isSelected()) {
+					date();
+				}else if(topics.isSelected()) {
+					topic();
+				}
+				else {
+					title();
+				}
+			}
+		});
 		
-		JRadioButton body = new JRadioButton("Body");
-		body.setFont(new Font("Calibri", Font.BOLD, 14));
-		body.setBounds(290, 92, 65, 21);
-		contentPane.add(body);
+		
+		
+		
+		
+		
+		
+		
+		srchBTN.setFont(new Font("Calibri", Font.BOLD, 15));
+		srchBTN.setBounds(396, 39, 108, 38);
+		contentPane.add(srchBTN);
+
+		
+		JLabel lblNewLabel = new JLabel("Resault :");
+		lblNewLabel.setForeground(Color.RED);
+		lblNewLabel.setFont(new Font("Calibri", Font.BOLD, 15));
+		lblNewLabel.setBounds(59, 146, 55, 19);
+		contentPane.add(lblNewLabel);
+		
+		
 		
 		JLabel lblNewLabel_1 = new JLabel("Search Filter :");
 		lblNewLabel_1.setForeground(Color.RED);
 		lblNewLabel_1.setFont(new Font("Calibri", Font.BOLD, 15));
 		lblNewLabel_1.setBounds(74, 89, 87, 27);
 		contentPane.add(lblNewLabel_1);
+		
+		JButton clean = new JButton("REFRESH");
+		clean.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				searchTxt.setText(" ");
+				contentsTxt.setText(" ");
+				
+			}
+		});
+		clean.setBackground(Color.YELLOW);
+		clean.setForeground(Color.BLACK);
+		clean.setFont(new Font("Calibri", Font.BOLD, 15));
+		clean.setBounds(514, 39, 108, 38);
+		contentPane.add(clean);
 
 	}
-}
+	public static void title() {
+		String temp="<TITLE>"+searchTxt.getText()+ "</TITLE>";
+		//System.out.println("Hello World");
+		
+        try {
+     	   for(int i=0;i<22;i++) {
+     			BufferedReader reader= new BufferedReader(new FileReader("src\\\\javasearchengine/read/reut2-"+i+".sgm"));
+     			StringBuilder strB=new StringBuilder();
+     			String str= "";
+     			StringBuilder testB=new StringBuilder();
+     			String body= "";
+     			
+     			do {
+     				str =reader.readLine();
+     				if(str==null)break;
+     						if(str.contentEquals(temp)) {
+     							strB.append(str);
+     							//contentsTxt.setText(strB.toString());
+     								do {									
+     								body=reader.readLine();
+     								if(body==null)break;
+     								testB.append(body);
+     								testB.append("\n");	
+     								ArrayList<String> bodyContents = new ArrayList<>();
+     								bodyContents.add(testB.toString());
+     												//System.out.println(body.toString());
+     								contentsTxt.setText(bodyContents.toString());
+     												if(body.contentEquals("&#3;</BODY></TEXT>")==true)break;	
+     								}while(body!=null);	
+     										
+     						}
+     			}while(str!=null);
+     			
+     	   }
+				}catch(Exception e1) {
+					JOptionPane.showInternalMessageDialog(null, "Error :"+e1.toString());
+				}
+		}
+	public static void date() {
+		String date="<DATE>"+searchTxt.getText()+ "</DATE>";//1;
+		//System.out.println("Hello World");
+		
+        try {
+     	   for(int i=0;i<22;i++) {
+     			BufferedReader reader= new BufferedReader(new FileReader("src\\\\javasearchengine/read/reut2-"+i+".sgm"));
+     			StringBuilder strB=new StringBuilder();
+     			String str= "";
+     			StringBuilder testB=new StringBuilder();
+     			String body= "";
+     			
+     			do {
+     				str =reader.readLine();
+     				if(str==null)break;
+     						if(str.contentEquals(date)) {
+     							strB.append(str);
+     							//contentsTxt.setText(strB.toString());
+     								do {									
+     								body=reader.readLine();
+     								if(body==null)break;
+     								testB.append(body);
+     								testB.append("\n");	
+     								ArrayList<String> bodyContents = new ArrayList<>();
+     								bodyContents.add(testB.toString());
+     												//System.out.println(body.toString());
+     								contentsTxt.setText(bodyContents.toString());
+     												if(body.contentEquals("&#3;</BODY></TEXT>")==true)break;	
+     								}while(body!=null);	
+     										
+     						}
+     			}while(str!=null);
+     			
+     	   }
+				}catch(Exception e1) {
+					JOptionPane.showInternalMessageDialog(null, "Error :"+e1.toString());
+				}
+		}
+	public static void topic() {
+		String topic="<TOPICS><D>"+searchTxt.getText()+ "</D></TOPICS>"; //<TOPICS><D>money-fx</D></TOPICS>
+		try {
+        	   for(int i=0;i<22;i++) {
+        			BufferedReader reader= new BufferedReader(new FileReader("src\\\\javasearchengine/read/reut2-"+i+".sgm"));
+        			StringBuilder strB=new StringBuilder();
+        			String str= "";
+        			StringBuilder testB=new StringBuilder();
+        			String body= "";
+        			
+        			do {
+        				str =reader.readLine();
+        				if(str==null)break;
+        						if(str.contentEquals(topic)) {
+        							strB.append(str);
+        							//contentsTxt.setText(strB.toString());
+        								do {									
+        								body=reader.readLine();
+        								if(body==null)break;
+        								testB.append(body);
+        								testB.append("\n");	
+        								ArrayList<String> bodyContents = new ArrayList<>();
+        								bodyContents.add(testB.toString());
+        												//System.out.println(body.toString());
+        								contentsTxt.setText(bodyContents.toString());
+        												if(body.contentEquals("&#3;</BODY></TEXT>")==true)break;	
+        								}while(body!=null);	
+        										
+        						}
+        			}while(str!=null);
+        			
+        	   }
+				}catch(Exception e1) {
+					JOptionPane.showInternalMessageDialog(null, "Error :"+e1.toString());
+				}
+		
+	}
+	}
